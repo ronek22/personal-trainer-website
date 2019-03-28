d8485f762891831e2721c570c0f1717d2bd99d29
ronek22 <jronek3010@gmail.com>
2019-03-29 00:58:52 +0100

    Sticky hiding on scroll down navigation added

--- js/main.js
+++ js/main.js
@@ -1,33 +1,38 @@
-// $(document).ready(function() {
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('nav').outerHeight();
 
-//     var toggleAffix = function(affixElement, scrollElement, wrapper) {
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
     
-//       var height = affixElement.outerHeight(),
-//           top = wrapper.offset().top;
-      
-//       if (scrollElement.scrollTop() >= top){
-//           wrapper.height(height);
-//           affixElement.addClass("affix");
-//       }
-//       else {
-//           affixElement.removeClass("affix");
-//           wrapper.height('auto');
-//       }
-        
-//     };
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
     
-  
-//     $('[data-toggle="affix"]').each(function() {
-//       var ele = $(this),
-//           wrapper = $('<div></div>');
-      
-//       ele.before(wrapper);
-//       $(window).on('scroll resize', function() {
-//           toggleAffix(ele, $(this), wrapper);
-//       });
-      
-//       // init
-//       toggleAffix(ele, $(window), wrapper);
-//     });
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('nav').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('nav').removeClass('nav-up').addClass('nav-down');
+        }
+    }
     
-//   });
+    lastScrollTop = st;
+}

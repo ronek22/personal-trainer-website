e7fa8312eb85e12244094d82beab9538af83921b
ronek22 <jronek3010@gmail.com>
2019-03-30 17:49:03 +0100

    Fix logo and add email service

--- js/main.js
+++ js/main.js
@@ -86,4 +86,11 @@ $(document).ready(function () {
             $('.short-gymnastic').fadeIn();
         });
     })
+
+    $('.nav-link').click(function() {
+        var sectionTo = $(this).attr('href');
+        $('html, body').animate({
+          scrollTop: $(sectionTo).offset().top
+        }, 1500);
+    });
 })
